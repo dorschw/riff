d1dd26d20f60a176070744fcc67c7e928e9ce161
dorschw <81086590+dorschw@users.noreply.github.com>
2023-08-11 17:05:03 +0300

    fix pytest pre-commit hook

--- riff/tests/riff_test.py
+++ riff/tests/riff_test.py
@@ -10,20 +10,28 @@ from riff.violation import Violation
     ("modified_lines", "expected_violation_count", "always_fail_on"),
     [
         pytest.param({}, 0, None, id="nothing changed"),
-        pytest.param({3}, 0,  None,id="unrelated lines changed"),
-        pytest.param({1}, 1,  None,id="violation 1 line changed"),
-        pytest.param({1,3}, 1, None, id="violation 1 line changed, and unrelated"),
-        pytest.param({1,2}, 2,  None,id="violation 1+2 lines changed"),
-        pytest.param({1,2,3}, 2,  None,id="violation 1+2 lines changed, and unrelated"),
-        pytest.param({}, 1,  {"E1"},id="nothing changed, always fail on E1"),
-        pytest.param({2}, 2,  {"E1"},id="line 1 changed, always fail on E2"),
-        pytest.param({}, 2,  {"E1","E2"},id="nothing changed, always fail on E1,E2"),
-        pytest.param({3}, 2,  {"E1","E2"},id="unrelated line changed, always fail on E1,E2"),
-        pytest.param({1,2}, 2,  {"E1","E2"},id="1+2 lines changed, always fail on E1,E2"),
+        pytest.param({3}, 0, None, id="unrelated lines changed"),
+        pytest.param({1}, 1, None, id="violation 1 line changed"),
+        pytest.param({1, 3}, 1, None, id="violation 1 line changed, and unrelated"),
+        pytest.param({1, 2}, 2, None, id="violation 1+2 lines changed"),
+        pytest.param(
+            {1, 2, 3}, 2, None, id="violation 1+2 lines changed, and unrelated"
+        ),
+        pytest.param({}, 1, {"E1"}, id="nothing changed, always fail on E1"),
+        pytest.param({2}, 2, {"E1"}, id="line 1 changed, always fail on E2"),
+        pytest.param({}, 2, {"E1", "E2"}, id="nothing changed, always fail on E1,E2"),
+        pytest.param(
+            {3}, 2, {"E1", "E2"}, id="unrelated line changed, always fail on E1,E2"
+        ),
+        pytest.param(
+            {1, 2}, 2, {"E1", "E2"}, id="1+2 lines changed, always fail on E1,E2"
+        ),
     ],
 )
 def test_filter_violations(
-    modified_lines: set[int], expected_violation_count: int, always_fail_on:set[str] | None
+    modified_lines: set[int],
+    expected_violation_count: int,
+    always_fail_on: set[str] | None,
 ) -> None:
     violations = [
         Violation(
@@ -44,9 +52,9 @@ def test_filter_violations(
     git_modified_lines = {
         Path("file1.py"): modified_lines,
     }
-    
+
     result = filter_violations(violations, git_modified_lines, always_fail_on)
     modified_lines_result = {violation.line_start for violation in result}
-    
+
     assert len(result) == expected_violation_count
     assert len(modified_lines_result) == expected_violation_count
