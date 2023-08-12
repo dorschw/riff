559dece7468b101140e8fb59b958050839d381e5
dorschw <81086590+dorschw@users.noreply.github.com>
2023-08-12 18:06:32 +0300

    forget tags

--- riff/release/check_ruff_version.py
+++ riff/release/check_ruff_version.py
@@ -1,28 +1,19 @@
 import requests  # type:ignore[import]
-from git.repo import Repo
 from loguru import logger  # noqa: TID251
 from packaging.version import Version
 
 
-def get_latest_ruff_version() -> Version:
-    response = requests.get("https://pypi.org/pypi/ruff/json", timeout=5).json()
+def get_latest_version(project: str) -> Version:
+    response = requests.get(f"https://pypi.org/pypi/{project}/json", timeout=5).json()
     return max(Version(version_string) for version_string in response["releases"])
 
 
-def get_latest_tag() -> Version:
-    repo = Repo(search_parent_directories=True)
-    logger.info(repo.tags)
-    return Version(
-        sorted(repo.tags, key=lambda t: t.commit.committed_datetime)[-1].name
-    )
+ruff_version = get_latest_version("ruff")
+riff_version = get_latest_version("riff")
 
-
-latest_ruff_version = get_latest_ruff_version()
-latest_riff_tag = get_latest_tag()
-
-logger.info(f"{latest_ruff_version=}")
-logger.info(f"{latest_riff_tag=}")
+logger.info(f"{ruff_version=}")
+logger.info(f"{riff_version=}")
 
 print(  # noqa: T201
-    f"::set-output name=newer_ruff_version::{latest_ruff_version > Version(latest_riff_tag.base_version)}"
+    f"::set-output name=newer_ruff_version::{ruff_version > Version(riff_version.base_version)}"
 )
