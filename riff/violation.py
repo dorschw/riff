c98b4d7e0d8c330b9ae51826f038e852c784ac6c
Kamal Qarain <kqarain@paloaltonetworks.com>
2024-12-04 18:44:16 +0200

    Fix Violation.to_github_annotation method

--- riff/violation.py
+++ riff/violation.py
@@ -32,13 +32,7 @@ class Violation(NamedTuple):
 
     def to_github_annotation(self: "Violation") -> str:
         relativized_path = self.path.relative_to(Path.cwd())
-        first_part = f"::error title=Ruff ({self.error_code}),file={self.path},line={self.line_start},"
-        "col={self.column_start},endLine={self.line_end},endColumn={self.column_end}::"
-
-        second_part = f"{relativized_path}:{self.line_start}:"
-        if self.column_start:
-            second_part += f"{self.column_start}:"
-        return f"{first_part}{second_part} {self.error_code} {self.message}"
+        return f'::error file={relativized_path},line={self.line_start},col={self.column_start},endLine={self.line_end},endColumn={self.column_end}::"Ruff ({self.error_code})"'
 
     @staticmethod
     def parse(raw: dict) -> "Violation":
